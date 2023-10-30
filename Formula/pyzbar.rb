3c5b0577ab1b4b256a2caf0961af427b6a8d9a8c
Jared Vacanti <jaredvacanti@gmail.com>
2023-10-29 22:53:48 -0500

    add sha256

--- Formula/pyzbar.rb
+++ Formula/pyzbar.rb
@@ -2,13 +2,14 @@ class Pyzbar < Formula
     include Language::Python::Virtualenv
 
     depends_on "zbar"
-    depends_on "python@3.10"
+    depends_on "python@3.11"
 
     desc "Read one-dimensional barcodes and QR codes from Python 2 and 3."
     homepage "https://github.com/NaturalHistoryMuseum/pyzbar"
     license "MIT"
 
     url "https://github.com/NaturalHistoryMuseum/pyzbar/archive/refs/tags/v0.1.9.tar.gz"
+    sha256 "6b84ec4c1f36df590c65b610a7757ef8829f202a7df210d2891e6592a8bee8bb"
     version "0.1.9"
 
     def install
